2972d3400b61659f6188f061f5d9cce896e441b2
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-12-18 12:03:40 +0900

    3章まで完了

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,70 @@
+import styles from "./page.module.css";
+import Image from "next/image";
+
+import NewsList from "@/app/_components/NewsList";
+import ButtonLink from "@/app/_components/ButtonLink";
+import { News } from "@/app/_libs/microcms";
+
+const data: { contents: News[] } = {
+  contents: [
+    {
+      id: "1",
+      title: "渋谷にオフィスを移転しました",
+      category: {
+        name: "更新情報",
+      },
+      publishedAt: "2023/05/19",
+      createdAt: "2023/05/29",
+    },
+    {
+      id: "2",
+      title: "当社CEOが業界リーダーTOP30に選出されました",
+      category: {
+        name: "更新情報",
+      },
+      publishedAt: "2023/05/19",
+      createdAt: "2023/05/29",
+    },
+    {
+      id: "3",
+      title: "テストの記事です",
+      category: {
+        name: "更新情報",
+      },
+      publishedAt: "2023/04/19",
+      createdAt: "2023/04/29",
+    },
+  ],
+};
+
 export default function Home() {
-  return <h1>テクノロジーのちからで世界を考える</h1>;
+  const name = "世界";
+  const sliceData = data.contents.slice(0, 2);
+  // const sliceData: News = [];
+  return (
+    <>
+      <section className={styles.top}>
+        <div>
+          <h1 className={styles.title}>テクノロジーのちからで{name}を考える</h1>
+          <p className={styles.description}>
+            私たちは市場をリードしているグローバルテックカンパニーです。
+          </p>
+        </div>
+        <Image
+          className={styles.bgimg}
+          src="/img-mv.jpg"
+          alt=""
+          width={4000}
+          height={1200}
+        />
+      </section>
+      <section className={styles.news}>
+        <h2 className={styles.newsTitle}>News</h2>
+        <NewsList news={sliceData} />
+        <div className={styles.newsLink}>
+          <ButtonLink href="/news">もっとみる</ButtonLink>
+        </div>
+      </section>
+    </>
+  );
 }
